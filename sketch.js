b4ed07815d51d901a57264fb78fbdeacbd98f3c1
AcherBecher <114704389+AcherBecher@users.noreply.github.com>
2022-10-25 17:47:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var sea,ship;
+var seaimg,shipImg;
+
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingship",shipImg1);
+  ship.sacle =0.25;
+}
+
+function draw() {
+  background("blue");
+ background(0);
+ sea.velocityX = -3;
+ drawSprites();
+}
